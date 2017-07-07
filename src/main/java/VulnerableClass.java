83d99059cc13a44c462b56602d7804832abc08ab
Alyson Fernandes Basilio <Alyson@Alyson-PC>
2017-07-06 21:14:38 -0300

    93% de cobertura de testes.

--- src/main/java/VulnerableClass.java
+++ src/main/java/VulnerableClass.java
@@ -8,55 +8,56 @@ import java.io.PrintWriter;
 import java.security.InvalidParameterException;
 
 public class VulnerableClass {
-	public void vulnerableMethod(String FILENAME, String opr, String texto){
-		if(FILENAME.length()<1){
-			throw new InvalidParameterException();
-		}
-	    if(opr.length()>1)
-	    	opr= opr.charAt(0)+"";
-	    if (opr.equals("W")){
-			try {
-				FileWriter arq = new FileWriter(FILENAME);
-				PrintWriter pWrite = new PrintWriter(arq);
-				pWrite.append(texto + "\n");
-			    pWrite.close();
-			    arq.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			} 
-		}
-		else{
-			System.out.println("Entrada Inválida.");
-			throw new InvalidParameterException();
-		}
+public void vulnerableMethod(String FILENAME, String opr, String texto){
+	if (FILENAME.length() < 1) {
+		throw new InvalidParameterException();
 	}
-	public String vulnerableMethod(String FILENAME, String opr){
-		if(FILENAME.length()<1){
-			throw new InvalidParameterException();
-		}
-		if(opr.length()>1)
-	    	opr= opr.charAt(0)+"";
-		if (opr.equals("R")){
-			try {
-				FileReader arq = new FileReader(FILENAME);
-				BufferedReader br = new BufferedReader(arq);
-				String result = "";
-				String aux;
-				while ((aux = br.readLine()) != null) {
-					System.out.println(".");
-					result += aux;
-				}
-				br.close();
-				arq.close();
-				return result;
-			} catch (IOException e) {
-				e.printStackTrace();
-			} 
-		}
-		else{
-			System.out.println("Entrada Inválida.");
-			throw new InvalidParameterException();
-		}
-	    return null;
+    if (opr.length() > 1) {
+    	opr = opr.charAt(0) + "";
+    }
+    if (opr.equals("W")) {
+    	try {
+			FileWriter arq = new FileWriter(FILENAME);
+			PrintWriter pWrite = new PrintWriter(arq);
+			pWrite.append(texto + "\n");
+		    pWrite.close();
+		    arq.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
 	}
+	else {
+		System.out.println("Entrada Invalida.");
+		throw new InvalidParameterException();
+	}
+}
+public String vulnerableMethod(String FILENAME, String opr){
+	if(FILENAME.length()<1){
+		throw new InvalidParameterException();
+	}
+	if(opr.length()>1)
+    	opr= opr.charAt(0)+"";
+	if (opr.equals("R")){
+		try {
+			FileReader arq = new FileReader(FILENAME);
+			BufferedReader br = new BufferedReader(arq);
+			String result = "";
+			String aux;
+			while ((aux = br.readLine()) != null) {
+				System.out.println(".");
+				result += aux;
+			}
+			br.close();
+			arq.close();
+			return result;
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+	}
+	else{
+		System.out.println("Entrada Invï¿½lida.");
+		throw new InvalidParameterException();
+	}
+    return null;
+}
 }
